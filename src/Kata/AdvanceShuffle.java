e9f993363f96662fc24baff8d631f147b17235e8
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-07-11 01:46:48 +0100

    AdvanceShuffle

--- src/Kata/AdvanceShuffle.java
+++ src/Kata/AdvanceShuffle.java
@@ -2,29 +2,73 @@ package Kata;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class AdvanceShuffle {
-    public static void main(String[] args) {
-        int number = 3;
-        int[] array = {1, 2, 3, 4};
+    public static void advanceShuffleForPositiveNumbers(int [] array,int number){
+        number%= array.length;
         int[] newArray = new int[array.length];
-        int arrayNumber = array.length - 1;
-        int count;
-        int counter =0;
-        while(counter < array.length) {
-
-            int fixNumber = array[arrayNumber];
-            System.out.print(fixNumber + " ");
-            for(count=number-1;count<0;count--) {
-                newArray[count] = fixNumber;
-
-            }
-//            for (int count = number - 1; count >= 0; count--) {
-//                newArray[count]=fixNumber;
-//                count--;
-
+        int counter = 0;
+        int count=1;
+        while (counter < number) {
+            int arrayNumber = array.length - count;
+            int arrayNumber2 = number - count;
+            int fixedNumber=array[arrayNumber];
+            newArray[arrayNumber2]=fixedNumber;
             counter++;
+            count++;
+
+        }
+        System.out.println(counter);
+        int c=0;
+        for(int county=number;county< array.length;county++){
+            newArray[county]=array[c];
+            c++;
+
         }
         System.out.println(Arrays.toString(newArray));
     }
+
+    public static void advanceShuffleForNegative(int[] array,int number){
+        number%=array.length;
+        int [] newArray=new int[array.length];
+        int counter=0;
+        int count=1;
+        while (counter < Math.abs(number)) {
+            int arrayNumber = (Math.abs(number)-1)- counter;
+            int arrayNumber2 = array.length - count;
+            int fixedNumber=array[arrayNumber];
+            newArray[arrayNumber2]=fixedNumber;
+            counter++;
+            count++;
+
+
+        }
+        int y= (array.length)-Math.abs(number);
+        int x=Math.abs(number);
+        for(int u=0;u<y;u++){
+
+            newArray[u]=array[x];
+
+            x++;
+
+        }
+
+        System.out.println(Arrays.toString(newArray));
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter you Preferred Number");
+        int number = scanner.nextInt();
+
+        int[] array = {1, 2, 3, 4,5,7,8,9,12};
+        if(number>0){
+    advanceShuffleForPositiveNumbers(array,number);
+}
+  else      advanceShuffleForNegative(array,number);
+
+}
 }
