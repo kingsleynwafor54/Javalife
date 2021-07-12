b9b1d92b4ec9fdb66456235174aeaae9ecbf2417
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-07-12 14:14:47 +0100

    StringStartAndEnd

--- src/ChapterFourteen/StringStartAndEnd.java
+++ src/ChapterFourteen/StringStartAndEnd.java
@@ -1,2 +1,32 @@
-package ChapterFourteen;public class StringStartAndEnd {
+package ChapterFourteen;
+
+import java.util.Locale;
+
+public class StringStartAndEnd {
+    public static void main(String[] args) {
+        String[] strings={"Started","Starting","ended","ending","state"};
+        for(int counter=0;counter<strings.length;counter++){
+            String st=strings[counter].toLowerCase(Locale.ROOT);
+            if(st.startsWith("st")){
+                System.out.println(strings[counter]);
+            }
+        }
+
+        System.out.println();
+
+        for(int counter=0;counter<strings.length;counter++){
+            String st=strings[counter].toLowerCase(Locale.ROOT);
+            if(st.startsWith("art",2)){
+                System.out.println(strings[counter]);
+            }
+        }
+
+        for(int counter=0;counter<strings.length;counter++){
+            String st=strings[counter].toLowerCase(Locale.ROOT);
+            if(st.endsWith("ed")){
+                System.out.println(strings[counter]);
+            }
+        }
+
+    }
 }
