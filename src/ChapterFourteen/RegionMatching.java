74308edd17aed7851b2c9110794008847474a608
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-07-12 13:49:15 +0100

    RegionMatching

--- src/ChapterFourteen/RegionMatching.java
+++ src/ChapterFourteen/RegionMatching.java
@@ -3,43 +3,23 @@ package ChapterFourteen;
 import java.util.Locale;
 
 public class RegionMatching {
-    public static int numberOfOccurrenceOfAStringInASentence(String word,String wordToMatch) {
-        int count = 0;
-        int start = 0;
-        int county = 0;
-       int counter=0;
-        String word2 = word.toLowerCase(Locale.ROOT);
-      for( count=0;count<word.length();count++){
-          for ( counter =count+ counter < word.length()+county; counter++) {
-                if (counter % 4 == 0) {
-                    System.out.println();
-                }
+    public static int numberOfOccurrenceOfAStringInASentence(String s2, String s1) {
+        String word=s2.toLowerCase(Locale.ROOT);
+        int counter = 0;
+        for (int count = 0; count < s2.length(); count++) {
+            if (word.regionMatches(count, s1, 0, 4)) {
 
-                System.out.print(word2.charAt(counter));
-                if (word.regionMatches(counter, wordToMatch, counter, 4)) {
-                    count++;
-                }
+                counter++;
+
             }
-
-            county++;
-            start++;
         }
-
-
-        System.out.println(start);
-        return count;
+        return counter;
     }
-        public static  void main(String... args){
-        String s1="Kojo";
-        String s2="bolaleKojo";
-        if(s2.regionMatches(0,s1,0,9)){
-            System.out.println("correct");
-        }
-        else
-            System.out.println("you lie");
 
+    public static void main(String... args) {
+        String s1 = "lale";
+        String s2 = "bolaleklale";
         System.out.println(numberOfOccurrenceOfAStringInASentence(s2,s1));
+
     }
-
-
 }
