253fcb7a5deae9d273826bdca37e311c1b2424a8
Kingsley Nwafor <kingsleynwafor54@gmail.com>
2021-07-13 13:56:49 +0100

    TokenizationOfSentence

---
+++ src/ChapterFourteen/TokenizationOfSentence.java
@@ -0,0 +1,19 @@
+package ChapterFourteen;
+
+public class TokenizationOfSentence {
+    public static void tokenizationOfSentence(String sentence){
+        int count=0;
+        String[] token=sentence.split(",");
+        for(int counter=0;counter<token.length;counter++){
+            System.out.println(token[counter]);
+            if(token[counter].equalsIgnoreCase( "side")){
+            count++;
+        }
+        }
+        System.out.println("The number of words in the sentence is "+count);
+    }
+
+    public static void main(String[] args) {
+        tokenizationOfSentence("hello,from,the,other,side");
+    }
+}
